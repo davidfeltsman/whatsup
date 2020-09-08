08b496db4df7465d80bd2dc4ba3c1b5a64cf656f
Vladislav Pyrko <vladislav.pyrko@gmail.com>
2020-09-08 22:45:56 +0300

    create base frontend structure, add materialIcons

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCjhsCItLeCTPS2s9Vi8ZF2Yx5D39gRDQk",
+  authDomain: "whatsup-mern-fc5a2.firebaseapp.com",
+  databaseURL: "https://whatsup-mern-fc5a2.firebaseio.com",
+  projectId: "whatsup-mern-fc5a2",
+  storageBucket: "whatsup-mern-fc5a2.appspot.com",
+  messagingSenderId: "119310384682",
+  appId: "1:119310384682:web:c5eaeeec80d8e78fc48d53"
+};
